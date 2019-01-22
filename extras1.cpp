6daa17c319d0f1e26c8ca85c0dce35df58c38236
andrewh <andrewtholt60@gmail.com>
2019-01-22 16:00:42 +0000

    Started to add c++ test class.

---
+++ extras1.cpp
@@ -0,0 +1,8 @@
+#include "extras1.h"
+
+smallest::smallest() {
+    std::cout << "Smallest" << std::endl;
+}
+
+
+
---
+++ extras1.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#ifdef __cplusplus
+extern "C" {
+#include "ficl.h"
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+class smallest {
+    smallest();
+};
+
