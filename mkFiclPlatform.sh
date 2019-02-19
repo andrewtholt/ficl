962e89601e5fd176f51a625004af12a387b8db4f
andrewh <andrewtholt60@gmail.com>
2019-02-19 07:40:26 +0000

    cmake runs mkFiclPlatform.sh

--- mkFiclPlatform.sh
+++ mkFiclPlatform.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # set -x
-
 DEST=ficlplatform.h
+echo "Creating $DEST"
 rm -f $DEST
 echo "#define FICL_PLATFORM_OS \"`uname`\"" >> $DEST
 echo "#define FICL_PLATFORM_ARCHITECTURE \"`uname -m`\"" >> $DEST
